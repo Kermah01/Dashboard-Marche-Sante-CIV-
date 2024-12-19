7811057b376204f5b6e2131cfd7d4daebbf81dd4
Kermah01 <126328483+Kermah01@users.noreply.github.com>
2024-12-19 20:09:31 +0000

    Update Santé.py

--- Santé.py
+++ Santé.py
@@ -32,7 +32,7 @@ df=read_excel_file("BD Santé 19 Décembre.xlsx")
 page_bg_img = f"""
     <style>
     [data-testid="stAppViewContainer"] > .main {{
-    background-image: url(https://www.mmpp.fr/uploads/HEADER-10.jpg);
+    background-image: url(https://cdn.futura-sciences.com/sources/images/qr/Celine/systeme%20sant%C3%A9%20%20LIGHTFIELD%20STUDIOS%2C%20Adobe%20Stock.jpeg);
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;

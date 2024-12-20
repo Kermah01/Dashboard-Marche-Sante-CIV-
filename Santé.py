62025da9426d8a26562df0e8902bfe81bb5e1b81
Kermah01 <126328483+Kermah01@users.noreply.github.com>
2024-12-20 11:47:48 +0000

    Update Santé.py

--- Santé.py
+++ Santé.py
@@ -32,7 +32,7 @@ df=read_excel_file("BD Santé 19 Décembre.xlsx")
 page_bg_img = f"""
     <style>
     [data-testid="stAppViewContainer"] > .main {{
-    background-image: url(https://static.vecteezy.com/system/resources/previews/003/462/449/non_2x/medical-technology-and-science-concept-and-health-care-background-free-vector.jpg);
+    background-image: url(https://cdn.futura-sciences.com/sources/images/qr/Celine/systeme%20sant%C3%A9%20%20LIGHTFIELD%20STUDIOS%2C%20Adobe%20Stock.jpeg);
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
